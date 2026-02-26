69700e660a4ad4b3052f54cd21fd3df17709e503
Bhargav <nakkabhargav20@gmail.com>
2026-02-26 21:24:52 +0530

    Fixed logout crash by converting logout to POST request

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from .models import StudyTask
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
 from django.utils import timezone
 
 class StudyTaskForm(forms.ModelForm):
