132c9feeea4c59ed7e9596c3f0e9b151cc131a01
Bhargav <nakkabhargav20@gmail.com>
2026-03-14 15:33:12 +0530

    Added password strength meter UI

--- core/forms.py
+++ core/forms.py
@@ -15,6 +15,13 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+    # remove password help text
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+
 
 # ---------------------------
 # Study Task Form
