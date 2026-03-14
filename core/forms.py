9409685afcd542210e099233b78d08b7279337ee
Bhargav <nakkabhargav20@gmail.com>
2026-03-14 17:07:43 +0530

    Improved UI design for login and registration pages

--- core/forms.py
+++ core/forms.py
@@ -13,23 +13,18 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username","email","password1","password2"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
-        # remove help text
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
 
-        # add bootstrap styling
         for field in self.fields.values():
             field.widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': ''
+                "class": "auth-input"
             })
-
-
 # ---------------------------
 # Study Task Form
 # ---------------------------
