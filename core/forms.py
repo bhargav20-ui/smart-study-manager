6cad48eb02ad192f76df663eeee76a254e91d2e6
Bhargav <nakkabhargav20@gmail.com>
2026-03-14 18:44:52 +0530

    Complete UI redesign

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,10 @@
 from django import forms
-from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import StudyTask
+from django.utils import timezone
 
 
-# ---------------------------
-# Registration Form
-# ---------------------------
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -18,47 +15,35 @@ class RegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # remove default help text
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
 
-        # apply bootstrap classes
-        self.fields['username'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Username'
-        })
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': ''
+            })
 
-        self.fields['email'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Email'
-        })
 
-        self.fields['password1'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Password'
-        })
-
-        self.fields['password2'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Confirm Password'
-        })
-# ---------------------------
-# Study Task Form
-# ---------------------------
 class StudyTaskForm(forms.ModelForm):
 
     class Meta:
         model = StudyTask
         fields = ['title', 'description', 'deadline']
+
         widgets = {
-            'deadline': forms.DateInput(attrs={'type': 'date'}),
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'description': forms.Textarea(attrs={'class':'form-control'}),
+            'deadline': forms.DateInput(attrs={
+                'type':'date',
+                'class':'form-control'
+            })
         }
 
     def clean_deadline(self):
         deadline = self.cleaned_data.get('deadline')
         today = timezone.now().date()
 
-        # Safety check in case deadline is empty
         if deadline and deadline < today:
             raise forms.ValidationError("Deadline cannot be in the past.")
 
--- core/views.py
+++ core/views.py
@@ -1,121 +1,153 @@
-from django.shortcuts import render, redirect
-from .forms import StudyTaskForm
-from .models import StudyTask
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
-
 from django.contrib.auth import login
-from .forms import RegisterForm
+from django.db.models import Q
+from .forms import RegisterForm, StudyTaskForm
+from .models import StudyTask
+
+
+def landing(request):
+    return render(request,"landing.html")
+
 
 def register(request):
+
     if request.method == "POST":
         form = RegisterForm(request.POST)
+
         if form.is_valid():
-            user = form.save()   # IMPORTANT: do not use commit=False
-            login(request, user)
+            user = form.save()
+            login(request,user)
             return redirect("dashboard")
+
     else:
         form = RegisterForm()
 
-    return render(request, "register.html", {"form": form})
+    return render(request,"register.html",{"form":form})
 
-# Create your views here.
+
 @login_required
 def dashboard(request):
-    query = request.GET.get('q')
-    filter_status = request.GET.get('status')
+
+    query = request.GET.get("q")
 
     tasks = StudyTask.objects.filter(user=request.user)
 
     if query:
         tasks = tasks.filter(
-            Q(title__icontains=query) | Q(description__icontains=query)
-            )
-
-    if filter_status == 'completed':
-        tasks = tasks.filter(completed=True)
-    elif filter_status == 'pending':
-        tasks = tasks.filter(completed=False)
+            Q(title__icontains=query) |
+            Q(description__icontains=query)
+        )
 
     total_tasks = tasks.count()
     completed_tasks = tasks.filter(completed=True).count()
 
+    progress = 0
+
     if total_tasks > 0:
-        progress = int((completed_tasks / total_tasks) * 100)
-    else:
-        progress = 0
-    
+        progress = int((completed_tasks/total_tasks)*100)
+
     context = {
-        "tasks": tasks,
-        "total_tasks": total_tasks,
-        "completed_tasks": completed_tasks,
-        "progress": progress,
+        "tasks":tasks,
+        "total_tasks":total_tasks,
+        "completed_tasks":completed_tasks,
+        "progress":progress
     }
-    return render(request, "home.html", context)
+
+    return render(request,"home.html",context)
+
 
 @login_required
 def add_task(request):
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = StudyTaskForm(request.POST)
+
         if form.is_valid():
             task = form.save(commit=False)
             task.user = request.user
             task.save()
-            return redirect('dashboard')
+
+            return redirect("dashboard")
+
     else:
         form = StudyTaskForm()
-    return render(request, 'add_task.html', {'form': form})
 
-@login_required
-def complete_task(request, task_id):
-    task = get_object_or_404(StudyTask, id=task_id, user=request.user)
-    task.completed = True
-    task.save()
-    return redirect('dashboard')
-
-@login_required
-def delete_task(request, task_id):
-    if request.method == "POST":
-        task = get_object_or_404(StudyTask, id=task_id, user=request.user)
-        task.delete()
-    return redirect('dashboard')
-
-def landing(request):
-    return render(request, "landing.html")
+    return render(request,"add_task.html",{"form":form})
 
 
 @login_required
-def toggle_task(request, task_id):
-    task = get_object_or_404(StudyTask, id=task_id, user=request.user)
-    task.completed = not task.completed
-    task.save()
-    return redirect('dashboard')
+def edit_task(request,task_id):
 
-@login_required
-def profile(request):
-    tasks = StudyTask.objects.filter(user=request.user)
-    total_tasks = tasks.count()
-    completed_tasks = tasks.filter(completed=True).count()
-    progress = int((completed_tasks / total_tasks) * 100) if total_tasks > 0 else 0
-
-    context = {
-        "total_tasks": total_tasks,
-        "completed_tasks": completed_tasks,
-        "progress": progress,
-    }
-    return render(request, "profile.html", context)
-
-@login_required
-def edit_task(request, task_id):
-    task = get_object_or_404(StudyTask, id=task_id, user=request.user)
+    task = get_object_or_404(
+        StudyTask,
+        id=task_id,
+        user=request.user
+    )
 
     if request.method == "POST":
-        form = StudyTaskForm(request.POST, instance=task)
+
+        form = StudyTaskForm(
+            request.POST,
+            instance=task
+        )
+
         if form.is_valid():
             form.save()
-            return redirect('dashboard')
+            return redirect("dashboard")
+
     else:
         form = StudyTaskForm(instance=task)
 
-    return render(request, "edit_task.html", {"form": form})
+    return render(request,"edit_task.html",{"form":form})
+
+
+@login_required
+def delete_task(request,task_id):
+
+    task = get_object_or_404(
+        StudyTask,
+        id=task_id,
+        user=request.user
+    )
+
+    task.delete()
+
+    return redirect("dashboard")
+
+
+@login_required
+def toggle_task(request,task_id):
+
+    task = get_object_or_404(
+        StudyTask,
+        id=task_id,
+        user=request.user
+    )
+
+    task.completed = not task.completed
+    task.save()
+
+    return redirect("dashboard")
+
+
+@login_required
+def profile(request):
+
+    tasks = StudyTask.objects.filter(user=request.user)
+
+    total_tasks = tasks.count()
+    completed_tasks = tasks.filter(completed=True).count()
+
+    progress = 0
+
+    if total_tasks > 0:
+        progress = int((completed_tasks/total_tasks)*100)
+
+    context = {
+        "total_tasks":total_tasks,
+        "completed_tasks":completed_tasks,
+        "progress":progress
+    }
+
+    return render(request,"profile.html",context)
